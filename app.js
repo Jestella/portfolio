8cd299fb62322cb0b79c887b8c1af8bf500e6976
jestella <jestellaa@gmail.com>
2021-06-25 21:49:49 -0400

    add The Intersection Observer API

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 'use strict';
 
-// Elements
+// Revealing project
+
+const allProjects = document.querySelectorAll('.project');
+
+const revealProject = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('project--hidden');
+  observer.unobserve(entry.target);
+};
+
+const projectObserver = new IntersectionObserver(revealProject, {
+  root: null,
+  threshold: 0.15,
+});
+
+allProjects.forEach(function (project) {
+  projectObserver.observe(project);
+  project.classList.add('project--hidden');
+});
+
+// Copyright year
 const currentYear = document.querySelector('.current_year');
 
 const date = new Date();
